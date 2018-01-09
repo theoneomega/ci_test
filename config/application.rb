b79eba39c9f36472f1ef9aa65d403b98509e7294
Joel Melendez Mata <theoneomega@gmail.com>
2018-01-09 14:59:55 -0700

    configure database

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,18 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Ci
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+	config.generators do |generate|
+	  generate.helper false
+	  generate.assets false
+	  generate.view_specs false
+	  generate.helper_specs false
+	  generate.routing_specs false
+	  generate.controller_specs false
+	end
   end
+  
 end
